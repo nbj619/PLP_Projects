74482c30e32e05285036254c5c5d8c3f88f3e285
nbj619 <nanaboachiejnr@gmail.com>
2024-02-24 08:22:42 +0000

    Initial commit

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name=input('What is your name?')
+age=int(input('How old are you?'))
+location=input('Where do you stay?')
+print('Welcome '+name+','+ 'you are '+str(age)+'years old '+'and you stay at '+location)
